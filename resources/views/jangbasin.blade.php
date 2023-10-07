4fc2b3c31dfeb5bca2a614b62e56b8a736941603
Pornnapa-Panyadee <pornnapa.van@gmail.com>
2023-10-07 22:01:01 +0700

    first commit

---
+++ resources/views/jangbasin.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Jang-Basin</title>
+  <link href='https://fonts.googleapis.com/css?family=Mitr' rel='stylesheet'>
+  <link rel="stylesheet" href="{{ asset('css/components.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/icons.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/responsee.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/template-style.css') }}">
+  <script type="text/javascript" src="{{ asset('js/jquery-1.8.3.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/jquery-ui.min.js') }}"></script>
+
+  <style>
+
+  </style>
+
+</head>
+
+
+<body class="size-1520 primary-color-red background-dark">
+
+
+  <!-- MAIN -->
+  <main role="main">
+    <!-- TOP SECTION -->
+    <section class="grid">
+      <!-- Main Carousel -->
+      <div
+        class="s-12 margin-bottom carousel-fade-transition owl-carousel carousel-main carousel-nav-white carousel-hide-arrows background-dark">
+        <div class="item background-image" style="background-image:url(images/bg-03.jpg)">
+          <p class="text-padding text-strong text-white text-s-size-30 text-size-60 text-uppercase background-primary">
+            ลุ่มน้ำแม่จาง
+            จังหวัดลำปาง </p>
+          <p class="text-padding text-size-20 text-dark text-uppercase background-white">
+            โครงการพัฒนาระบบการสำรวจและบริหารจัดการพื้นที่เสี่ยงภัยน้ำท่วมและภัยแล้ง
+            บนพื้นฐานของเทคโนโลยีสารสนเทศและการจัดการขั้นสูง</p>
+        </div>
+      </div>
+    </section>
+
+    <!-- SECTION 1 -->
+    <section class="grid margin text-center">
+      <a href="https://watercenter.scmc.cmu.ac.th/weir/jang_basin/"
+        class="s-12 m-6 l-6 padding-2x vertical-center margin-bottom background-blue" target=”_blank”>
+        <i class="icon-sli-map text-size-60 text-white center margin-bottom-15"></i>
+        <h3 class="text-strong text-size-20 text-line-height-1 margin-bottom-30 text-uppercase">
+          ระบบสารสนเทศการตรวจประเมินและวางแผน <br>ปรับปรุงเพิ่มประสิทธิภาพฝาย
+        </h3>
+      </a>
+      <a href="https://watercenter.scmc.cmu.ac.th/blockage/jang_basin/"
+        class="s-12 m-6 l-6 padding-2x vertical-center margin-bottom background-aqua " target=”_blank”>
+        <i class="icon-sli-layers text-size-60 text-white center margin-bottom-15"></i>
+        <h3 class="text-strong text-size-20 text-line-height-1 margin-bottom-30 text-uppercase">
+          ระบบข้อมูลสารสนเทศของสิ่งกีดขวางทางน้ำในลำน้ำ <br>คูคลองและถนนที่มีปัญหา
+          การกีดขวางทางน้ำ
+
+        </h3>
+      </a>
+    </section>
+
+
+  </main>
+
+
+  <!-- FOOTER -->
+  <footer class="grid background-white text-center margin-bottom">
+    <div class="s-12 m-9 l-4 padding-2x background-white">
+      <div class="grid margin">
+        <img class="s-4 m-4 margin-bottom" src="images/lampang.png" style="width: 150px;" />
+        <img class="s-4 m-4 margin-top" src="images/egat.jpg" style="width: 250px;" />
+        <img class="s-4 m-4 margin-bottom" src="images/cmu.png" style="width:150px;" />
+
+      </div>
+    </div>
+    <div class="s-12 m-9 l-6 padding-2x background-white">
+      <h2 class="text-orange text-strong text-uppercase">ติดต่อเรา</h2>
+      <p>ศูนย์ความเป็นเลิศด้านการจัดการภัยพิบัติทางธรรมชาติ คณะวิศวกรรมศาสตร์ มหาวิทยาลัยเชียงใหม่
+      </p>
+      <p>ที่อยู่ :239 ถนนห้วยแก้ว ต.สุเทพ อ.เมือง จ.เชียงใหม่ 50200 โทร : 053-942074</p>
+      <p>Facebook : <a class="text-primary-hover" href="https://www.facebook.com/CENDiMcmu">CENDiMcmu</a>
+        | Website : <a class="text-primary-hover" href="https://cendim.eng.cmu.ac.th/">www.cendim.eng.cmu.ac.th</a></p>
+    </div>
+
+  </footer>
+  <script type="text/javascript" src="{{ asset('js/responsee.js') }}"></script>
+  <!-- <script type="text/javascript" src="{{ asset('js/template-scripts.js') }}"></script> -->
+
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,198 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+	<!-- Meta Tags -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="Site keywords here">
+	<meta name="description" content="">
+	<meta name='copyright' content=''>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Title -->
+	<title>WaterCenter</title>
+
+	<!-- Favicon -->
+	<link rel="icon" href="img/favicon.png">
+
+	<!-- Google Fonts -->
+	<link href='https://fonts.googleapis.com/css?family=Mitr' rel='stylesheet'>
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+	<!-- Nice Select CSS -->
+	<!-- <link rel="stylesheet" href="css/nice-select.css"> -->
+	<!-- Font Awesome CSS -->
+	<link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+	<!-- icofont CSS -->
+	<link rel="stylesheet" href="{{ asset('css/icofont.css') }}">
+	<!-- Owl Carousel CSS -->
+	<link rel="stylesheet" href="{{ asset('css/owl-carousel.css') }}">
+
+	<!-- Medipro CSS -->
+	<link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+
+</head>
+
+<body>
+	<!-- Header Area -->
+	<header class="header">
+		<!-- Header Inner -->
+		<div class="header-inner">
+			<div class="container">
+				<div class="inner">
+					<div class="row">
+						<div class="col-lg-3 col-md-3 col-12">
+							<!-- Start Logo -->
+							<div class="logo">
+								<h3><i class="icofont-water-drop" style="color: #1A76D1;"></i>
+									<font style="color: #1A76D1;">Water</font>center
+								</h3>
+							</div>
+							<!-- End Logo -->
+							<!-- Mobile Nav -->
+							<div class="mobile-nav"></div>
+							<!-- End Mobile Nav -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/ End Header Inner -->
+	</header>
+	<!-- End Header Area -->
+
+	<!-- Slider Area -->
+	<section class="slider">
+		<div class="hero-slider">
+			<!-- Start Single Slider -->
+			<div class="single-slider" style="background-image:url('images/slider5.jpg')">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-8">
+							<div class="text" align="right">
+								<h1>
+									ศูนย์วิชาการสนับสนุนการ<span>บริหารจัดการน้ำ</span></h1>
+								<h3>มหาวิทยาลัยเชียงใหม่ </h3>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- End Single Slider -->
+		</div>
+	</section>
+	<!--/ End Slider Area -->
+
+	<!-- Start Schedule Area -->
+	<section class="schedule">
+		<div class="container">
+			<div class="schedule-inner">
+				<div class="row">
+					<div class="col-lg-4 col-md-6 col-12 ">
+						<!-- single-schedule -->
+						<div class="single-schedule first">
+							<div class="inner">
+								<div class="icon">
+									<i class="fa fa-industry"></i>
+								</div>
+								<div class="single-content">
+									<h4>ลุ่มน้ำจาง จังหวัดลำปาง</h4>
+									<p>โครงการพัฒนาระบบการสำรวจและบริหารจัดการพื้นที่เสี่ยงภัยน้ำท่วมและภัยแล้ง
+										บนพื้นฐานของเทคโนโลยีสารสนเทศและการจัดการขั้นสูงในพื้นที่ลุ่มน้ำแม่จาง
+										จังหวัดลำปาง <br>
+										<i class="fa fa-hand-o-right" aria-hidden="true"></i>
+										โดยการไฟฟ้าฝ่ายผลิตแห่งประเทศไทย (กฟผ) แม่เมาะ
+										ร่วมกับมหาวิทยาลัยเชียงใหม่
+									</p>
+
+									<a href="/jang-basin">เว็บไซต์<i class="fa fa-long-arrow-right"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 col-12">
+						<!-- single-schedule -->
+						<div class="single-schedule middle">
+							<div class="inner">
+								<div class="icon">
+									<i class="icofont-prescription"></i>
+								</div>
+								<div class="single-content">
+									<h4>Chiang Rai Weir Assessment System</h4>
+									<p>โครงการพัฒนาระบบสารสนเทศการตรวจประเมินสภาพฝายและวางแผน
+										ปรับปรุงเพิ่มประสิทธิภาพฝายในพื้นที่จังหวัดเชียงราย <br>
+										<i class="fa fa-hand-o-right" aria-hidden="true"></i>
+										โดยจังหวัดเชียงรายร่วมกับมหาวิทยาลัยเชียงใหม่
+									</p>
+									<a href="https://weir.crflood.com/" target="_blank">เว็บไซต์<i
+											class="fa fa-long-arrow-right"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-12 col-12">
+						<!-- single-schedule -->
+						<div class="single-schedule last">
+							<div class="inner">
+								<div class="icon">
+									<i class="icofont-ui-clock"></i>
+								</div>
+								<div class="single-content">
+									<h4>Chiang Rai Steam Blockage</h4>
+									<p>โครงการพัฒนาระบบสารสนเทศการตรวจประเมินสภาพฝายและวางแผนปรับปรุงเพิ่มประสิทธิภาพฝายในพื้นที่จังหวัดเชียงราย
+										<br><i class="fa fa-hand-o-right" aria-hidden="true"></i>
+										โดยจังหวัดเชียงรายร่วมกับมหาวิทยาลัยเชียงใหม่
+									</p><br>
+									<a href="https://blockage.crflood.com/" target="_blank">เว็บไซต์<i
+											class="fa fa-long-arrow-right"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--/End Start schedule Area -->
+
+
+	<!-- Footer Area -->
+	<footer id="footer" class="footer ">
+
+		<!--/ End Footer Top -->
+		<!-- Copyright -->
+		<div class="copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 col-md-12 col-12">
+						<div class="copyright-content">
+							<p>ศูนย์วิชาการสนับสนุนการบริหารจัดการน้ำ มหาวิทยาลัยเชียงใหม่ </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/ End Copyright -->
+	</footer>
+	<!--/ End Footer Area -->
+
+	<!-- jquery Min JS -->
+	<script src="{{ asset('js/jquery.min.js') }}"></script>
+	<!-- jquery Migrate JS -->
+	<script src="{{ asset('js/jquery-migrate-3.0.0.js') }}"></script>
+	<!-- jquery Ui JS -->
+	<script src="{{ asset('js/jquery-ui.min.js') }}"></script>
+	<!-- Slicknav JS -->
+	<script src="{{ asset('js/slicknav.min.js') }}"></script>
+	<!-- Owl Carousel JS -->
+	<!-- <script src="js/owl-carousel.js"></script> -->
+	<!-- Main JS -->
+	<script src="{{ asset('js/main.js') }}"></script>
+</body>
+
+</html>
